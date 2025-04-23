df753f5201a792ec828e1c0961a8aa9d31cdcce0
irenenguyen <mpn1008@gmail.com>
2025-04-23 15:18:11 +0200

    flyway

---
+++ dbis/src/main/resources/db/migration/V1_0__mock_flyway.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS testtable
+(
+    buyer_id           BIGINT PRIMARY KEY,
+    email              VARCHAR(320) NOT NULL,
+    password           CHAR(96)     NULL,
+    buyer_name         TEXT         NOT NULL,
+    social_id          TEXT         NOT NULL,
+    social_platform    TEXT         NOT NULL,
+    status             TEXT         NOT NULL,
+    created_at         TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at         TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by         TEXT         NOT NULL,
+    updated_by         TEXT         NOT NULL
+);
