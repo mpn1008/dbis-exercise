049c4352e6e3e3d2d02f3ebb0bec7003ac9e0920
irenenguyen <mpn1008@gmail.com>
2025-04-23 11:06:02 +0200

    message

---
+++ dbis/src/main/java/com/example/dbis/DbisApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbisApplication.class, args);
+	}
+
+}
---
+++ dbis/src/test/java/com/example/dbis/DbisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
